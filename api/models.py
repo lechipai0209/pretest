a4037b219272eb9b19a770fab405c5666f0cdebb
kimery <king0209king0209@gmail.com>
2025-11-23 01:23:52 +0800

    feat: add import_api_token

--- api/models.py
+++ api/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Order(models.Model):
-    # Add your model here
-    pass
+    order_number = models.AutoField(primary_key=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.order_number
 
--- api/views.py
+++ api/views.py
@@ -13,5 +13,29 @@ ACCEPTED_TOKEN = ('omni_pretest_token')
 
 @api_view(['POST'])
 def import_order(request):
-    # Add your code here
-    return HttpResponseBadRequest()
+
+    total_price = request.data.get('total_price')
+    if total_price is None :        
+        return JsonResponse(
+            {"error": "total_price must be a valid decimal number"},
+            status=400
+        )
+
+    try : 
+        total_price = Decimal(total_price)
+
+    except(InvalidOperation, TypeError) :
+        return JsonResponse(
+            {"error": "total_price must be a valid decimal number"},
+            status=400
+        )
+    
+    order = Order.objects.create(total_price=total_price)
+    
+    return JsonResponse({
+        "message": "Order imported successfully",
+        "order_number": order.order_number,
+        "total_price": str(order.total_price),
+        "created_time": order.created_time,
+    }, status=200)
+
