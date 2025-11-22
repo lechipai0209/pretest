b5d470fe1873e6d3c14b8e4576b5e820a2c305b6
kimery <king0209king0209@gmail.com>
2025-11-23 02:31:57 +0800

    feat: add product model and build relation with order model

--- api/models.py
+++ api/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
+class Product(models.Model) :
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-# Create your models here.
+    def __str__(self):
+        return str(self.name)
+
 class Order(models.Model):
     order_number = models.AutoField(primary_key=True)
+    product =  models.ForeignKey(Product, on_delete=models.CASCADE, related_name="orders")
+    product_amount = models.IntegerField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.order_number
+        return str(self.order_number)
 
